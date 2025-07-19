6611a82009fc48e3c4fc80c1c7fc26ec46f00f7b
titosco <titobiloluwatella@gmail.com>
2025-07-19 13:37:08 +0100

    done

--- web-prop/src/app/sections/Cards.jsx
+++ web-prop/src/app/sections/Cards.jsx
@@ -1,8 +1,79 @@
+import Image from 'next/image'
 import React from 'react'
+import Img1 from '../images/img1.png';
+import Img2 from '../images/img2.png';
+import Img3 from '../images/img3.png';
+import Img4 from '../images/img4.png';
 
 const Cards = () => {
   return (
-    <div className=''></div>
+    <div className='mt-4 grid relative md:grid-cols-2 md:gap-20 lg:grid-cols-4 lg:gap-6'>
+        {/* card for 4 listings */}
+        {/* card1 */}
+        <div className='card'>
+            <div className="card-inner">
+                <div className="card-face">
+                    <Image alt='' src={Img1} className='img-front'/>
+                    <div className='m-4'>
+                        <span className='card-title'>
+                            Redesigning my site increased sales by over 400% in one week.
+                        </span>
+                        <span className='card-desc'>
+                            Jul 1,2020
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        {/* card 2 */}
+               <div className='card'>
+            <div className="card-inner">
+                <div className="card-face">
+                    <Image alt='' src={Img2} className='img-front'/>
+                    <div className='m-4'>
+                        <span className='card-title'>
+                            Design handoff: What engineers really want to see.
+                        </span>
+                        <span className='card-desc'>
+                            Jul 1,2020
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        {/* card 3 */}
+               <div className='card'>
+            <div className="card-inner">
+                <div className="card-face">
+                    <Image alt='' src={Img3} className='img-front'/>
+                    <div className='m-4'>
+                        <span className='card-title'>
+                            A comprehensive list of human-computer interactions
+                        </span>
+                        <span className='card-desc'>
+                            Jul 1,2020
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        {/* card 4 */}
+               <div className='card'>
+            <div className="card-inner">
+                <div className="card-face">
+                    <Image alt='' src={Img4} className='img-front'/>
+                    <div className='m-4'>
+                        <span className='card-title'>
+                            Why do poeple hate Redesigning?
+                        </span>
+                        <span className='card-desc'>
+                            Jul 1,2020
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
--- web-prop/src/app/sections/Section2.jsx
+++ web-prop/src/app/sections/Section2.jsx
@@ -3,7 +3,7 @@ import Cards from './Cards'
 
 const Section2 = () => {
   return (
-    <main className='pt-4 sm:pt-8 w-full items-center justify-center flex'>
+    <main className='pt-4 sm:pt-8 w-full items-center justify-center flex flex-col'>
        <div className='flex items-center w-full max-w-md mx-auto'>
             <span className='flex-1 h-px bg-gray-400'></span>
             <span className='px-4 text-xs sm:text-sm text-primary text-center whitespace-nowrap'>
@@ -12,9 +12,9 @@ const Section2 = () => {
             <span className='flex-1 h-px bg-gray-400'></span>
         </div>
         {/* cards */}
-        {/* <div className='flex flex-col mx-auto p-2 max-x-6xl gap-8 my-5'>
+        <div className='flex flex-col mx-auto p-2 max-x-6xl gap-8 my-5'>
             <Cards />
-        </div> */}
+        </div>
     </main>
   )
 }
